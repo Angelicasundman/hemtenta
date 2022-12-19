6b54e81f0f2a23be78af163cea01e4e53411c78c
Angelicasundman <Angelicasundman@hotmail.com>
2022-12-19 20:44:50 +0100

    first commit

---
+++ uppgift 6/js.js
@@ -0,0 +1,19 @@
+let inputValue = document.querySelector('input.number');
+
+
+
+inputValue.addEventListener('keyup', e=>{
+  if ((inputValue === isNaN)) {
+    return 'not a number';
+  }
+  else {
+  reult = (inputValue*2)
+      return document.getElementById("demo").innerHTML = result;
+  }
+})
+
+
+
+
+  
+  
